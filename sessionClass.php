c34870a2e3f6facc43dd037f0dd10c33d2b8cf32
qooiverson3 <45475431+qooiverson3@users.noreply.github.com>
2019-07-14 21:27:31 +0800

    Update sessionClass.php

--- sessionClass.php
+++ sessionClass.php
@@ -1,5 +1,23 @@
 <?php
-include_once 'wesleyUtils.php';
+
+class student{
+    private $ch,$eng,$math;
+    public function __construct($ch,$eng,$math){
+           $this->ch = $ch;
+           $this->eng = $eng;
+           $this->math = $math;
+    }
+    public function sum(){
+        return $this->ch + $this->eng + $this->math;
+    }
+    public function avg(){
+        return $this->sum() / 3;
+    }
+    public function setCh($newch){
+        $this->ch = $newch;
+    }
+}
+
 session_start();
 $var1 = new student(90,88,89);
 echo "Sum = {$var1->sum()}<br>Avg = {$var1->avg()}<br>";
